60e2f5d126e72c9add4181d97f449065369b9cf0
JeongWonLim <cdnljw@gmail.com>
2022-07-18 13:10:27 +0900

    개별 거래실적 collector

--- src/collector/krx.js
+++ src/collector/krx.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const axios = require('axios');
-const express = require('express');
+let requestObj = {};
 
-var req= exports = modules.export = {};
+var req = exports = module.exports = {};
 
 req.ohlcvRequest = async function (stkcode, startDay, endday){
     return await axios(
@@ -27,3 +27,29 @@ req.ohlcvRequest = async function (stkcode, startDay, endday){
         }
     )
 }
+
+req.individuellTradingPerpormenceRequest = async function (stkcode, startDay, endday){
+    return await axios(
+        {
+            method : 'post',
+            url : '/bldAttendant/getJsonData.cmd',
+            baseURL : 'http://data.krx.co.kr/comm',
+            headers :{
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+                'X-Requested-With': 'XMLHttpRequest',
+            },
+            params : {
+                bld: 'dbms/MDC/STAT/standard/MDCSTAT02301',
+                isuCd: 'KR7005930003',
+                strtDd: '20220715',
+                endDd: '20220715',
+            },
+            transformResponse : function(data){
+                return JSON.parse(data).output;
+            }
+        }
+    )
+}
+
+
+req.individuellTradingPerpormenceRequest(1,2,3).then(data=>console.log(data.data))
--- test.js
+++ test.js
@@ -1,3 +1,38 @@
-test2 = require('./test2');
+axios = require('axios');
 
-console.log(test2);
+a =  function(){
+    console.log('a');
+    c = axios(
+        {
+            method : 'post',
+            url : '/bldAttendant/getJsonData.cmd',
+            baseURL : 'http://data.krx.co.kr/comm',
+            headers :{
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+                'X-Requested-With': 'XMLHttpRequest',
+            },
+            params : {
+                bld: 'dbms/MDC/STAT/standard/MDCSTAT01701',
+                isuCd: 'KR7005930003',
+                strtDd: '20220715',
+                endDd: '20220715',
+            },
+            transformResponse : function(data){
+                return JSON.parse(data).output;
+            }
+        }
+    )
+    console.log('a2');
+    console.log('a2');
+    console.log('a2');
+    console.log('a2');
+    console.log('a2');
+    console.log('a2');
+    console.log('a2');
+    console.log('a2');
+    console.log('a2');
+    return c}
+d = a();
+console.log(d);
+console.log('1212121111111111111');
+console.log('1212121')
